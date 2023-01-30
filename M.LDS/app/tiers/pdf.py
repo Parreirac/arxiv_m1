c4cc492614dde61159f3fb0103e852449e3adf42
tof <christophe.parreira-de-abrantes@student-cs.fr>
2023-01-30 22:59:51 +0100

    Code Style (STY)

--- M.LDS/app/tiers/pdf.py
+++ M.LDS/app/tiers/pdf.py
@@ -1,28 +1,39 @@
-from PyPDF2 import PdfFileReader
+""" Pdf handling """
 import logging
 
+from PyPDF2 import PdfReader
 
 logger = logging.getLogger(__name__)
-# https://pypi.org/project/PyPDF2/
-def getWatermark(filename):
-    pdf = PdfFileReader(filename)  #, strict=False ??? test file's existence by caller todo with ????
-    logger.info("getWatermark for {}".format(filename))
-    page = pdf.getPage(0)  # T
 
-    text =page.extract_text(90)
+
+def get_watermark(filename):
+    """vertically reads the watermark on the first page """
+    pdf = PdfReader(filename)  # , strict=False ??? test file's existence by caller todo with ????
+    logger.info("getWatermark for %s", filename)
+    page = pdf.pages[0]
+    text = page.extract_text(90)
 
     return text
 
-def getMetadata(filename):
-    document = PdfFileReader(filename) #   open(myFile, 'rb'))
-    metadata = document.getDocumentInfo()
-    print(metadata)
 
-    print(document.getNumPages())
+def get_text(filename):
+    """Get full pdf text"""
+    pdf = PdfReader(filename)  # , strict=False ??? test file's existence by caller todo with ????
+    logger.info("getText for %s", filename)
+    text = ""
+    for _i, page in enumerate(pdf.pages):
+        text = text + page.extract_text()
 
-    print(document.getFields())
+    return text
 
+
+def get_metadata(filename):
+    """Get pdf metadata"""
+    document = PdfReader(filename)  # open(myFile, 'rb'))
+    metadata = document.metadata
+    # print(metadata)
+    # print(len(document.pages))
+    # print(document.get_fields())
+    #    Le type d’affichage avec getPageLayout()
+    #    Le mode d’affichage avec getPageMode()
     return metadata
-
-#    Le type d’affichage avec getPageLayout()
-#    Le mode d’affichage avec getPageMode()
